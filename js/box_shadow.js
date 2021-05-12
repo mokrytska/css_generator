0adf56c23d2195e62eb88ae6289adf326f72270c
Maryanna <prelecnala@gmail.com>
2021-05-12 23:49:54 +0300

    box shadow

--- js/box_shadow.js
+++ js/box_shadow.js
@@ -4,9 +4,25 @@ function initBoxShadow () {
     let InputVertLength = document.querySelector('.generator__input--vertical-length');
     let InputBlurRadius = document.querySelector('.generator__input--Blur-radius');
     let InputSpread = document.querySelector('.generator__input--spread');
+    let InputHex = document.querySelector('.generator__input--Hex')
 
 InputHorLength.addEventListener('input', function () {
     exampleBlock.style.BoxShadowHorisontalLength = InputHorLength.value + 'px';
 })
 
+InputVertLength.addEventListener('input', function () {
+    exampleBlock.style.BoxShadowVerticalLength = InputVertLength.value + 'px';
+})
+
+InputBlurRadius.addEventListener('input', function () {
+    exampleBlock.style.BoxShadowBlurRadiius = InputBlurRadius.value + 'px';
+})
+
+InputSpread.addEventListener('input', function () {
+    exampleBlock.style.BoxShadowSpread = InputSpread.value + 'px';
+
+InputHex.addEventListener('input', function () {
+    exampleBlock.style.BoxShadowColor = InputHex.value;
+})
+})
 }
