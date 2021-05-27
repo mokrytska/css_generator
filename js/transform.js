e09f406ba55ac2d43f643ca8178d59c637b34eee
Maryanna <prelecnala@gmail.com>
2021-05-27 23:23:47 +0300

    transform is working

--- js/transform.js
+++ js/transform.js
@@ -7,7 +7,7 @@ function initTransform() {
     let inputSkewX = document.querySelector('.generator__item--transform .generator__input--skew_x');
     let inputSkewY = document.querySelector('.generator__item--transform .generator__input--skew_y');
     let exampleBlock = document.querySelector('.generator__item--transform .generated-block');
-    console.log(exampleBlock);
+    let textCopy = document.querySelector('.generator__item--transform .generator__styles');
 
 
     inputScale.addEventListener('input', setstyle);
@@ -18,19 +18,53 @@ function initTransform() {
     inputSkewY.addEventListener('input', setstyle);
 
 
-
-
-    
-function setstyle() {
-    let styleValue = ` 
-    scale( ${inputScale.value}) 
+    function setstyle() {
+        let styleValue = ` 
+    scale(${inputScale.value}) 
     rotate(${inputRotate.value}deg)
     translateX(${inputTranslateX.value}px)
     translateY(${inputTranslateY.value}px)
     skewX(${inputSkewX.value}deg)
     skewY(${inputSkewY.value}deg)
     `
-    console.log(styleValue.value);
-    exampleBlock.style.transform = styleValue;
-}
+        exampleBlock.style.transform = styleValue;
+        generateStyles ();
+    }
+
+    function generateStyles () {
+        textCopy.innerHTML = `
+        -moz-transform:
+        scale(${inputScale.value}) 
+        rotate(${inputRotate.value}deg)
+        translateX(${inputTranslateX.value}px)
+        translateY(${inputTranslateY.value}px)
+        skewX(${inputSkewX.value}deg)
+        skewY(${inputSkewY.value}deg)<br>
+        -webkit-transform: scale(${inputScale.value}) 
+        rotate(${inputRotate.value}deg)
+        translateX(${inputTranslateX.value}px)
+        translateY(${inputTranslateY.value}px)
+        skewX(${inputSkewX.value}deg)
+        skewY(${inputSkewY.value}deg);<br>
+        -o-transform:scale(${inputScale.value}) 
+        rotate(${inputRotate.value}deg)
+        translateX(${inputTranslateX.value}px)
+        translateY(${inputTranslateY.value}px)
+        skewX(${inputSkewX.value}deg)
+        skewY(${inputSkewY.value}deg) ;<br>
+        -ms-transform: scale(${inputScale.value}) 
+        rotate(${inputRotate.value}deg)
+        translateX(${inputTranslateX.value}px)
+        translateY(${inputTranslateY.value}px)
+        skewX(${inputSkewX.value}deg)
+        skewY(${inputSkewY.value}deg);<br>
+        transform: scale(${inputScale.value}) 
+        rotate(${inputRotate.value}deg)
+        translateX(${inputTranslateX.value}px)
+        translateY(${inputTranslateY.value}px)
+        skewX(${inputSkewX.value}deg)
+        skewY(${inputSkewY.value}deg)
+    `;
+    }
+    generateStyles ();
 }
