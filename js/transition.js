18d17a1b2c00f866c924769f0b9445c7fa09944e
Maryanna <prelecnala@gmail.com>
2021-06-16 19:25:37 +0300

    changes feat 5

--- js/transition.js
+++ js/transition.js
@@ -5,13 +5,29 @@ function initTransition() {
     let exampleBlock = document.querySelector('.generator__item--transition .generated-block');
     let textCopy = document.querySelector('.generator__item--transition .generator__styles');
 
-    inputProperty.addEventListener('input', function() {
+    inputProperty.addEventListener('input', function () {
         if (inputProperty.value === 'Background') {
             exampleBlock.style.background = 'green';
         }
+        if (inputProperty.value === 'Color') {
+            exampleBlock.style.color = 'red';
+        }
+        if (inputProperty.value === 'Height') {
+            exampleBlock.style.height = '150px';
+        }
+        if (inputProperty.value === 'Width') {
+            exampleBlock.style.width = '250px';
+        }
+        if (inputProperty.value === 'Outline') {
+            exampleBlock.style.outline = '5px solid pink';
+        }
+        if (inputProperty.value === 'All') {
+            exampleBlock.style. = '5px solid pink';
+        }
+
     })
 
-    inputDuration.addEventListener('input', function() {
+    inputDuration.addEventListener('input', function () {
         exampleBlock.style.transitinDiration = inputDuration.value + 'seconds';
     })
 
